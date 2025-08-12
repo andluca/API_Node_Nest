ab3882ca73f00ebda054bba04c5f9da7b1cfefe9
andvolveluca <109103844+andluca@users.noreply.github.com>
2025-08-12 03:20:51 -0300

    feat: implement JwtAuthGuard for JWT authentication with public route handling

---
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,35 @@
+import {
+  Injectable,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { User } from 'src/modules/users/entities/user.entity';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+
+  handleRequest<TUser = User>(err: any, user: TUser): TUser {
+    if (err || !user) {
+      throw err || new UnauthorizedException('Token inválido ou expirado');
+    }
+    return user;
+  }
+}
