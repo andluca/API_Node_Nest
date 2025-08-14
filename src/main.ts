d727620f11dec71fd61a519b1fea7001ad6a263f
andvolveluca <109103844+andluca@users.noreply.github.com>
2025-08-13 21:28:28 -0300

    fix: update default port from 3000 to 5000 in main.ts

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 5000;
   await app.listen(port);
 
   console.log(`API rodando em: http://localhost:${port}`);
